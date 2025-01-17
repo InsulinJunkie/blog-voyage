b6675e7e2885eaa5343b387081475dba4d509ef9
SoftEngie <studentsoftwareengineerulb@gmail.com>
2025-01-17 18:48:34 +0100

    Finished Astro tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fleur-de-cel.netlify.app/"
+  site: "https://fleur-de-cel.netlify.app/",
+  integrations: [preact()]
 });
