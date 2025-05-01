61988dca01b36461a959fb971949ff180c9cac32
SoftEngie <studentsoftwareengineerulb@gmail.com>
2025-04-30 21:07:34 -0400

    build: add tailwindcss package

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fleur-de-cel.netlify.app/",
-  integrations: [preact()]
+  site: "https://gaufreetsiropderable.netlify.app/",
+  integrations: [preact()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue,md,mdx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        remachine: ["'Remachine Script'"],
+      },
+    },
+  },
+  plugins: [],
+};
